4482e866f9490e05d022b8b784c5449b7ca44bf7
Ana Helena <anahelenarp@hotmail.com>
2021-08-15 15:28:21 -0300

    Add social links icons

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const Links = [
+    {
+        label: "Github",
+        url: "https://github.com/anahelenasilva"
+    },
+    {
+        label: "Twitter",
+        url: "https://twitter.com/anaahelena"
+    },
+    {
+        label: "Instagram",
+        url: "https://www.instagram.com/anassilvas"
+    }
+]
+
+export default Links;
