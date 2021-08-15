0b1ade5ba6e5b9da0415968f8699a67565644a71
Ana Helena <anahelenarp@hotmail.com>
2021-08-15 08:38:11 -0300

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Ana Helena's Blog`,
+    description: `This is my blog`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://github.com/anahelenasilva/iamanahelena-blog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
