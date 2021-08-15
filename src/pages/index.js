b49b1fa24c6149beb4607e91a6f609b7ebbf878c
Ana Helena <anahelenarp@hotmail.com>
2021-08-15 19:05:01 -0300

    Map post items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,55 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Home</h1>
-    <PostItem
-      category="misc"
-      date="August 15th 2021"
-      description="ddaasd daskdshdjksa ddjashdkjas dsakj sdjhcbzxnbz"
-      title="dsassssss"
-      slug="/about/"
-      timeToRead="5"
-    />
-  </Layout >
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+          },
+        }) => (
+          <PostItem
+            slug="/about/"
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
