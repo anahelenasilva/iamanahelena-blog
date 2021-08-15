13072d411feb360b3809c126486358b8b41b6e31
Ana Helena <anahelenarp@hotmail.com>
2021-08-15 12:59:11 -0300

    Add global styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 
 import PropTypes from "prop-types"
 import Profile from "../Profile"
+import GlobalStyles from "../../styles/global"
 
 const LayoutWrapper = styled.section`
   display: flex;
@@ -19,6 +20,7 @@ const Layout = ({ children }) => {
 
   return (
     <LayoutWrapper>
+      <GlobalStyles />
       <aside>
         <Profile />
       </aside>
