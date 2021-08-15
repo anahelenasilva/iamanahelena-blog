8920b7e319353169237ec39d9e529a281a02be63
Ana Helena <anahelenarp@hotmail.com>
2021-08-15 09:16:05 -0300

    Using graphql playground

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -4,7 +4,7 @@ const Profile = () => (
     <div className="Profile-wrapper">
         <h1>Ana Helena</h1>
         <h2>Desenvolvedora Fullstack</h2>
-        <p>Trabalho na MAG-IT e escrevo principalmente sobre desenvolvimento backend e docker</p>
+        <p>Trabalho na MAG-IT e escrevo principalmente sobre desenvolvimento backend e docker.</p>
     </div>
 )
 
