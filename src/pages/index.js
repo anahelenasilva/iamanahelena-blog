bba7fc719c7cb4926acf961688841788bc50b2a8
Ana Helena <anahelenarp@hotmail.com>
2021-08-15 16:25:58 -0300

    Postitem

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import * as React from "react"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Home</h1>
+    <PostItem
+      category="misc"
+      date="August 15th 2021"
+      description="ddaasd daskdshdjksa ddjashdkjas dsakj sdjhcbzxnbz"
+      title="dsassssss"
+      slug="/about/"
+      timeToRead="5"
+    />
   </Layout >
 )
 
