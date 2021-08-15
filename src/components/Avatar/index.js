f7eba595c7d700edc04309075e919670c0f9879e
Ana Helena <anahelenarp@hotmail.com>
2021-08-15 14:59:27 -0300

    Add style to Avatar component

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import * as S from "./styled"
 
 const Avatar = () => {
-    const { avatarImage } = useStaticQuery(
-        graphql`
+  const { avatarImage } = useStaticQuery(
+    graphql`
       query {
         avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
           childImageSharp {
@@ -15,9 +15,9 @@ const Avatar = () => {
         }
       }
     `
-    )
+  )
 
-    return <Img fixed={avatarImage.childImageSharp.fixed} />
+  return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fixed} />
 }
 
 export default Avatar
