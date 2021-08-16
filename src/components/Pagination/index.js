b1f618ce7e3767df8d1b384aa31628167706ad76
Ana Helena <anahelenarp@hotmail.com>
2021-08-16 08:45:29 -0300

    Edit texts

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -13,11 +13,11 @@ const Pagination = ({
     nextPage,
 }) => (
     <S.PaginationWrapper>
-        {!isFirst && <Link to={prevPage}>← página anterior</Link>}
+        {!isFirst && <Link to={prevPage}>← previous page</Link>}
         <p>
-            {currentPage} de {numPages}
+            {currentPage} of {numPages}
         </p>
-        {!isLast && <Link to={nextPage}>proxima página →</Link>}
+        {!isLast && <Link to={nextPage}>next page →</Link>}
     </S.PaginationWrapper>
 )
 
