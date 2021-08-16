81b6e4486d462e0c017a7f633aeae5fd7fc2182f
Ana Helena <anahelenarp@hotmail.com>
2021-08-16 08:37:30 -0300

    Add pagination component

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -2,37 +2,53 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
-import Seo from "../components/seo"
+import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
+import Pagination from '../components/Pagination'
 
-const BlogList = ({ data }) => {
-    const postList = data.allMarkdownRemark.edges
+const BlogList = props => {
+  const postList = props.data.allMarkdownRemark.edges
 
-    return (
-        <Layout>
-            <Seo title="Home" />
-            {postList.map(
-                ({
-                    node: {
-                        frontmatter: { background, category, date, description, title },
-                        timeToRead,
-                        fields: { slug },
-                    },
-                }) => (
-                    <PostItem
-                        key={slug}
-                        slug={slug}
-                        background={background}
-                        category={category}
-                        date={date}
-                        timeToRead={timeToRead}
-                        title={title}
-                        description={description}
-                    />
-                )
-            )}
-        </Layout>
-    )
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? '/' : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+            fields: { slug },
+          },
+        }) => (
+          <PostItem
+            key={slug}
+            slug={slug}
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </Layout>
+  )
 }
 
 export const query = graphql`
